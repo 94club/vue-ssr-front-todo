47abfbe1cd739b1500048915f46ed67af4dbd39b
yzijun <yzijun@126.com>
2017-08-18 16:08:49 +0800

    增加url-loader的配置

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -34,7 +34,15 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.(png|jpg|gif|svg)$/,
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: '[name].[ext]?[hash]'
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
